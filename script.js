5beb0ec1d12be4467f0463d56000ccfd243d715a
jerhickpancho26 <jerhickpancho26@gmail.com>
2026-03-02 15:57:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function openModal(src){
+document.getElementById("modal").style.display="flex";
+document.getElementById("modalImg").src=src;
+}
+
+function closeModal(){
+document.getElementById("modal").style.display="none";
+}
+
+
